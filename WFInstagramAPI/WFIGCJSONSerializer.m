2eb0766dd0c110b949497ba557190b22c41d04c5
Will Fleming <will@jwock.org>
2012-01-14 13:04:07 -0500

    trying to detect & use TouchJSON when avail. ARC is making trouble.

--- WFInstagramAPI/WFIGCJSONSerializer.m
+++ WFInstagramAPI/WFIGCJSONSerializer.m
@@ -6,17 +6,20 @@
 
 #import "WFIGCJSONSerializer.h"
 
-#import "CJSONDeserializer.h"
-#import "CJSONSerializer.h"
-
 @implementation WFIGCJSONSerializer
 
 + (id) deserializeJSON:(NSData*)jsonData error:(NSError*__autoreleasing*)error {
-  return [[CJSONDeserializer deserializer] deserialize:jsonData error:error];
+  Class cjsonDeserializer = NSClassFromString(@"CJSONDeserializer");
+  WFIGDASSERT(nil != cjsonDeserializer);  // should only be called when CJSON is available
+  id deserializer = (id __autoreleasing)[cjsonDeserializer deserializer];
+  return [deserializer deserialize:jsonData error:error];
 }
 
 + (NSData*) serializeJSON:(id)object error:(NSError* __autoreleasing*)error {
-  return [[CJSONSerializer serializer] serializeObject:object error:error];
+  Class cjsonSerializer = NSClassFromString(@"CJSONSerializer");
+  WFIGDASSERT(nil != cjsonSerializer);  // should only be called when CJSON is available
+  id serializer = (id __autoreleasing)[cjsonSerializer  serializer];
+  return [serializer serializeObject:object error:error];
 }
 
 @end
